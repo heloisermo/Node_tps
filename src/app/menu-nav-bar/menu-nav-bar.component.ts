ef4cda7eeb6ab751440ec644476362465d5683e2
heloisermo <heloise.romeo@edu.devinci.fr>
2024-11-25 18:08:14 +0100

    lien test page 1 fonctionne pas

--- src/app/menu-nav-bar/menu-nav-bar.component.ts
+++ src/app/menu-nav-bar/menu-nav-bar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-menu-nav-bar',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, RouterModule],
   templateUrl: './menu-nav-bar.component.html',
   styleUrl: './menu-nav-bar.component.css'
 })
